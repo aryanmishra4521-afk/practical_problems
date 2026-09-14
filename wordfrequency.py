eb7c26bf48b031779a646d785f3541f970b2461e
Aryan Kumar Mishra <aryanmishra4521@gmail.com>
2026-09-14 15:21:28 +0530

    Update wordfrequency.py

--- wordfrequency.py
+++ wordfrequency.py
@@ -5,8 +5,8 @@ count = 0
 for word in words:
    if word in frequency:
       frequency[word] += 1
-else:
+   else:
       frequency[word] = 1
 for word, count in frequency.items():
- print(word + ":", count)
+    print(word, ":", count)
 print(frequency)
