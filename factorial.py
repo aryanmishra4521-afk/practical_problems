630127fe450d38e0ec68d4f7b8d051eb85ebcf26
Aryan Kumar Mishra <aryanmishra4521@gmail.com>
2026-09-21 14:51:21 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+num = int(input("Enter a number to calculate its factorial: "))
+if num < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    print("The factorial of", num, "is", factorial(num))
